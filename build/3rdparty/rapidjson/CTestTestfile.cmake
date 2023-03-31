f419ba9a3d8d208d6968da9a1997089cfbc7775f
jhapreis <jhapreis@pop-os.localdomain>
2023-03-31 10:47:55 -0300

    removing builds

--- build/3rdparty/rapidjson/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jhapreis/Documents/IC/C-Arapuca-Open-New/3rdparty/rapidjson
-# Build directory: /home/jhapreis/Documents/IC/C-Arapuca-Open-New/build/3rdparty/rapidjson
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doc")
-subdirs("example")
-subdirs("test")
